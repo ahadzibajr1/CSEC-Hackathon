840a4a67f9d5f699c244a7908a416efe684d0433
Amila Hadžibajramović <you@example.com>
2024-02-24 15:30:41 +0100

    Add backend setup

---
+++ csec-hackathon/src/main/java/seaa/csechackathon/CsecHackathonApplication.java
@@ -0,0 +1,13 @@
+package seaa.csechackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsecHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsecHackathonApplication.class, args);
+	}
+
+}
---
+++ csec-hackathon/src/test/java/seaa/csechackathon/CsecHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package seaa.csechackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsecHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
